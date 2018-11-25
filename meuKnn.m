4dfefc063fd48c32d0788e3ddb737a58d1328f33
André Luiz Lunelli <to.lunelli@gmail.com>
2018-11-25 14:14:34 -0200

    alterado indice

--- meuKnn.m
+++ meuKnn.m
@@ -12,6 +12,6 @@ for j=1:size(dadosTeste)(1)
     _dist(j,i) = dist(dadosTeste(j,:), dadosTrain(i, :));
   endfor
   [distOrdenada, ind] = sort(_dist(j,:));
-  rotuloPrevisto(j,1) = rotuloTrain(ind(1:k));
+  rotuloPrevisto(j,1) = rotuloTrain(ind(k));
 endfor
 endfunction
